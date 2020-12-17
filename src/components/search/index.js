602f9dcfa9d6d5df47ac5b7cca3354ae0f4c68fd
eleviven <firuz.haciyeff@gmail.com>
2020-12-17 04:45:47 +0400

    Search component created

---
+++ src/components/search/index.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { Input } from "../";
+
+export default function Search({ invert, ...props }) {
+  const [text, setText] = useState("");
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <Input.Group {...props}>
+        <Input.Control
+          type="text"
+          value={text}
+          invert={invert}
+          addon={true}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Hansı oyunu axtarırsınız?"
+        />
+        <Input.Addon variant="primary">
+          <i className="far fa-search"></i>
+        </Input.Addon>
+      </Input.Group>
+    </form>
+  );
+}

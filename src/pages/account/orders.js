164011a26957660422657e21803a6c0457c9092e
eleviven <firuz.haciyeff@gmail.com>
2020-12-21 13:19:47 +0400

    orders page completed

--- src/pages/account/orders.js
+++ src/pages/account/orders.js
@@ -1,7 +1,13 @@
 import { Layout, Container, Hero, Paragraph, Avatar } from "@/components";
 import { withAuth } from "@/hoc";
+import { useOrders } from "@/graphql/actions/order.action";
 
 function Orders({ user }) {
+  const { data, loading } = useOrders({
+    variables: { id: user?.id },
+  });
+  const orders = data?.user?.orders;
+  const status = (con) => (con ? "Yükləndi" : "Gözləyir");
   return (
     <Layout hideServices={true}>
       <Hero>
@@ -22,25 +28,29 @@ function Orders({ user }) {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>#230</td>
-                <td>
-                  <div className="d-flex align-items-center">
-                    <Avatar
-                      src="/uploads/small_223dab8574bda5433484c505dddfde63_a7e6023a21.jpg"
-                      size="sm"
-                    />
-                    <div className="pl-3">
-                      <strong className="d-block mb-1">Pubg Mobile</strong>
-                      <small className="text-one">
-                        <strong>500 UC</strong>
-                      </small>
+              {orders?.map((order) => (
+                <tr key={order.id}>
+                  <td>#{order.order_code}</td>
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <Avatar
+                        src={order.game?.thumbnail?.formats?.small.url}
+                        size="sm"
+                      />
+                      <div className="pl-3">
+                        <strong className="d-block mb-1">Pubg Mobile</strong>
+                        <small className="text-one">
+                          <strong>
+                            {order.prices.in_game_value} {order.game.game_money}
+                          </strong>
+                        </small>
+                      </div>
                     </div>
-                  </div>
-                </td>
-                <td>9.99 AZN</td>
-                <td>Gözləyir</td>
-              </tr>
+                  </td>
+                  <td>{order.prices.value} AZN</td>
+                  <td>{status(order.status)}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>

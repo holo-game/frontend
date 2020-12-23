6d06332f30503e3785e408fd5bc158779a229cb2
eleviven <firuz.haciyeff@gmail.com>
2020-12-24 01:00:27 +0400

    Modified

--- src/components/panel/index.js
+++ src/components/panel/index.js
@@ -1,9 +1,11 @@
 import PanelWrapper from "./panel-wrapper";
 import PanelHeader from "./panel-header";
 import PanelBody from "./panel-body";
+import PanelFooter from "./panel-footer";
 
 export default {
   Wrapper: PanelWrapper,
   Header: PanelHeader,
   Body: PanelBody,
+  Footer: PanelFooter,
 };
--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -1,4 +1,14 @@
-import { Layout, Container, Hero, Paragraph } from "@/components";
+import { signOut } from "@/graphql/actions/auth.action";
+import {
+  Layout,
+  Container,
+  Hero,
+  Paragraph,
+  Button,
+  Row,
+  Col,
+  Panel,
+} from "@/components";
 import { withAuth } from "@/hoc";
 
 function Account({ user }) {
@@ -10,6 +20,33 @@ function Account({ user }) {
           icon={<i className="far fa-ghost text-six"></i>}
         />
       </Hero>
+      <section className="pb-sm-5 pb-3">
+        <Container>
+          <Row>
+            <Col className="col-sm-5">
+              <Panel.Wrapper>
+                <Panel.Body>
+                  <div className="mb-3">
+                    <label className="d-block">Username</label>
+                    <strong>{user.username}</strong>
+                  </div>
+                  <div>
+                    <label className="d-block">E-Mail</label>
+                    <strong>{user.email}</strong>
+                  </div>
+                </Panel.Body>
+                <Panel.Footer>
+                  <Button
+                    title="Çıxış"
+                    variant="danger"
+                    onClick={() => signOut()}
+                  />
+                </Panel.Footer>
+              </Panel.Wrapper>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Layout>
   );
 }

1a0d932383c5e2531887b22c4db9645869ef36b5
eleviven <firuz.haciyeff@gmail.com>
2020-12-28 02:17:51 +0400

    Add cookie parse method

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -30,4 +30,16 @@ export const requestAlert = ({
   });
 };
 
+export const parseCookie = (name, str) => {
+  var nameEQ = name + "=";
+  var ca = str && str.split(";");
+  if (!ca) return null;
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
 export const isLoggedInVar = makeVar(false);

1233169127b4ce76e2d9e95fb3731fcbcf868467
eleviven <firuz.haciyeff@gmail.com>
2020-12-30 06:15:32 +0400

    Add meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { APP_NAME, APP_DESCRIPTION } from "@/constants";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,8 +10,29 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="az">
-        <Head />
-        <link rel="manifest" href="/manifest.json" />
+        <Head>
+          <meta name="application-name" content={APP_NAME} />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+          <meta name="description" content={APP_DESCRIPTION} />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#272b3a" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/images/icons/icon-180x180"
+          />
+          <link rel="manifest" href="/manifest.json" />
+        </Head>
         <body>
           <Main />
           <NextScript />

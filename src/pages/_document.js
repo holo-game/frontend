6832eded25f57b128b24f9ec1e8de23d9a6141b1
eleviven <firuz.haciyeff@gmail.com>
2021-01-06 19:32:22 +0400

    Move head to _app

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { APP_NAME, APP_DESCRIPTION, LIVE_SUPPORT } from "@/constants";
+import { LIVE_SUPPORT } from "@/constants";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,29 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="az">
-        <Head>
-          <meta name="application-name" content={APP_NAME} />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-          <meta name="description" content={APP_DESCRIPTION} />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="theme-color" content="#272b3a" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/images/icons/icon-180x180"
-          />
-          <link rel="manifest" href="/manifest.json" />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
